3ca64c078da9b00eba46aca824a455814de30833
Chris Sperry <christophermartinsperry@gmail.com>
2019-01-08 22:48:21 +0000

    fix typo

--- docker-get-build-artefacts.sh
+++ docker-get-build-artefacts.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p ./docker-build-artefacts
-docker exec dynamicd cat dist/dynamicd >> ./docker-build-artefacts/dynamicd
-docker exec dynamicd cat dist/dynamic-cli >> ./docker-build-artefacts/dynamic-cli
+docker exec dynamicd cat dist/dynamicd > ./docker-build-artefacts/dynamicd
+docker exec dynamicd cat dist/dynamic-cli > ./docker-build-artefacts/dynamic-cli
 chmod +x ./docker-build-artefacts/dynamicd
 chmod +x ./docker-build-artefacts/dynamic-cli
