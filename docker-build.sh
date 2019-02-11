29dd2b8f3df8360ab58b537cb24a8f5a65934e20
Chris Sperry <christophermartinsperry@gmail.com>
2019-02-11 16:04:41 +0000

    update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
-./update-submodules.sh
+echo $1
+usage() { 
+    echo "docker-build.sh [-r]" 1>&2; 
+    echo " -r update submodule from remote" 1>&2
+    exit 1; 
+}
+while getopts "r" o; do
+    case "${o}" in
+        r)
+            echo "updating submodules from remote"
+            UPDATE_REMOTE="--remote";;
+        *)
+            usage
+            ;;
+    esac
+done
+./update-submodules.sh ${UPDATE_REMOTE}
+
 docker build --rm -f "dockerfile" -t dynamicd-testing:latest .
