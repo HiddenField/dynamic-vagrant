f3927529dd4243c1dd61f412dc86e7fedf310bed
Chris Sperry <christophermartinsperry@gmail.com>
2019-03-13 12:57:25 +0000

    add functionality to allow running dynamicd with -reindex -rescan

--- docker/entry.sh
+++ docker/entry.sh
@@ -9,6 +9,20 @@ trap exit_script SIGTERM
 
 PID_FILE=${HOME}/.dynamic/privatenet/dynamicd.pid
 (rm ${PID_FILE} || true) &>/dev/null 
+REINDEX_MARKER_FILE=${HOME}/.dynamic_reindex
+(rm ${REINDEX_MARKER_FILE} || true) &>/dev/null 
+
+stop_dynamicd() {
+    echo "stopping dynamicd";
+    dynamic-cli stop;
+    while [ -f "${PID_FILE}" ]
+    do
+        echo "waiting for pidfile to be deleted";
+        sleep 1;
+    done
+    echo "dynamicd is terminated";
+}
+
 if [ ! -f ~/.dynamic/dynamic.conf ]; then
     cp /dynamic/dynamic.default.conf ~/.dynamic/dynamic.conf
 fi
@@ -16,16 +30,15 @@ while [ -z ${STOPPING+x} ]
 do
     if [ ! -f "${PID_FILE}" ]; then
         echo "no pid, starting dynamicd";
-        dynamicd --daemon;
+        dynamicd -daemon;
+        echo "dynamicd pid is $(cat ${PID_FILE})";
+    fi
+    if [ -f "${REINDEX_MARKER_FILE}" ]; then
+        (rm ${REINDEX_MARKER_FILE} || true) &>/dev/null 
+        stop_dynamicd
+        dynamicd -daemon -reindex -rescan;
         echo "dynamicd pid is $(cat ${PID_FILE})";
     fi
     sleep 1;
 done
-echo "stopping dynamicd";
-dynamic-cli stop;
-while [ -f "${PID_FILE}" ]
-do
-    echo "waiting for pidfile to be deleted";
-    sleep 1;
-done
-echo "dynamicd is terminated";
+stop_dynamicd
---
+++ docker/reindex.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+REINDEX_MARKER_FILE=${HOME}/.dynamic_reindex
+touch ${REINDEX_MARKER_FILE}
